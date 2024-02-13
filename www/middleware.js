dde0a8d99453222981fba438424a9912197ef37c
Daniel Bach <danik.bachnov@gmail.com>
2024-02-13 20:54:44 +0200

    Changed to removeItem for clarity

--- www/middleware.js
+++ www/middleware.js
@@ -12,32 +12,10 @@ const promiseMiddleware = store => next => action => {
     store.dispatch({ type: ASYNC_START, subtype: action.type });
 
     const currentView = store.getState().viewChangeCounter;
-    const skipTracking = action.skipTracking;
 
     action.payload.then(
-      res => {
-        const currentState = store.getState()
-        if (!skipTracking && currentState.viewChangeCounter !== currentView) {
-          return
-        }
-        console.log('RESULT', res);
-        action.payload = res;
-        store.dispatch({ type: ASYNC_END, promise: action.payload });
-        store.dispatch(action);
-      },
-      error => {
-        const currentState = store.getState()
-        if (!skipTracking && currentState.viewChangeCounter !== currentView) {
-          return
-        }
-        console.log('ERROR', error);
-        action.error = true;
-        action.payload = error.response.body;
-        if (!action.skipTracking) {
-          store.dispatch({ type: ASYNC_END, promise: action.payload });
-        }
-        store.dispatch(action);
-      }
+      res => handlePromiseResolution(store, action, currentView, res),
+      error => handlePromiseRejection(store, action, currentView, error)
     );
 
     return;
@@ -47,14 +25,19 @@ const promiseMiddleware = store => next => action => {
 };
 
 const localStorageMiddleware = store => next => action => {
-  if (action.type === REGISTER || action.type === LOGIN) {
-    if (!action.error) {
-      window.localStorage.setItem('jwt', action.payload.user.token);
-      agent.setToken(action.payload.user.token);
-    }
-  } else if (action.type === LOGOUT) {
-    window.localStorage.setItem('jwt', '');
-    agent.setToken(null);
+  switch (action.type) {
+    case REGISTER:
+    case LOGIN:
+      if (!action.error) {
+        const { token } = action.payload.user;
+        window.localStorage.setItem('jwt', token);
+        agent.setToken(token);
+      }
+      break;
+    case LOGOUT:
+      window.localStorage.removeItem('jwt');
+      agent.setToken(null);
+      break;
   }
 
   next(action);
@@ -64,5 +47,33 @@ function isPromise(v) {
   return v && typeof v.then === 'function';
 }
 
+function isPromise(v) {
+  return v && typeof v.then === 'function';
+}
 
-export { promiseMiddleware, localStorageMiddleware }
+function handlePromiseResolution(store, action, currentView, res) {
+  const currentState = store.getState();
+  if (!action.skipTracking && currentState.viewChangeCounter !== currentView) {
+    return;
+  }
+  console.log('RESULT', res);
+  action.payload = res;
+  store.dispatch({ type: ASYNC_END, promise: action.payload });
+  store.dispatch(action);
+}
+
+function handlePromiseRejection(store, action, currentView, error) {
+  const currentState = store.getState();
+  if (!action.skipTracking && currentState.viewChangeCounter !== currentView) {
+    return;
+  }
+  console.log('ERROR', error);
+  action.error = true;
+  action.payload = error.response.body;
+  if (!action.skipTracking) {
+    store.dispatch({ type: ASYNC_END, promise: action.payload });
+  }
+  store.dispatch(action);
+}
+
+export { promiseMiddleware, localStorageMiddleware };
